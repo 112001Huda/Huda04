08e7683590d5bda3106e1141c5d6611cf84b08bc
inamd <inamdarhuda2001@gmail.com>
2024-05-19 13:53:18 +0530

    first commit

---
+++ ProgrammingPractice/src/Java8_Stream/StreamReduceExample.java
@@ -0,0 +1,18 @@
+package Java8_Stream;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class StreamReduceExample {
+    public static void main(String[] args) {
+        // Creating a list of integers
+        List<Integer> numbers = Arrays.asList(7, 2, 6, 9, 4, 3, 1);
+
+        // Summing all numbers
+        int sum = numbers.stream()
+                .reduce(0, (a, b) -> a + b);
+
+        System.out.println("Sum: " + sum); // Output: 32
+    }
+}
+
