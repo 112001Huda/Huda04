a6d7998672fe4faf6d0e3512ef7b5e30f2b7e56a
inamd <inamdarhuda2001@gmail.com>
2024-05-19 13:47:27 +0530

    first commit

---
+++ ProgrammingPractice/src/Java8_Stream/StreamCreationExample.java
@@ -0,0 +1,18 @@
+package Java8_Stream;
+
+
+
+    import java.util.stream.Stream;
+
+    public class StreamCreationExample {
+        public static void main(String[] args) {
+            // Creating a stream with multiple integers
+            Stream<Integer> streamOfNumbers = Stream.of(7, 2, 6, 9, 4, 3, 1);
+
+            // Counting elements in the stream
+            long count = streamOfNumbers.count();
+            System.out.println("Count: " + count); // Output: 7
+        }
+    }
+
+
