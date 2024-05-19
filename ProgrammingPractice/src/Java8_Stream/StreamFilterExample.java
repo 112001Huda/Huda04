903532092bc9404e871026347f366cd000e68426
inamd <inamdarhuda2001@gmail.com>
2024-05-19 15:20:15 +0530

    first commit

---
+++ ProgrammingPractice/src/Java8_Stream/StreamFilterExample.java
@@ -0,0 +1,19 @@
+package Java8_Stream;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StreamFilterExample {
+    public static void main(String[] args) {
+        // Creating a list of integers
+        List<Integer> numbers = Arrays.asList(7, 2, 6, 9, 4, 3, 1);
+
+        // Filtering out even numbers
+        List<Integer> oddNumbers = numbers.stream()
+                .filter(n -> n % 2 != 0)
+                .collect(Collectors.toList());
+
+        System.out.println("Odd Numbers: " + oddNumbers); // Output: [7, 9, 3, 1]
+    }
+}
